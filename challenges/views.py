0cd0f0558664283065cb17fe49c9d7c774bf6a8a
bhavanathakare09 <bhavanathakare09@gmail.com>
2025-06-16 15:51:06 -0400

    Dynamic code

--- challenges/views.py
+++ challenges/views.py
@@ -1,42 +1,37 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseNotFound
 
 # Create your views here.
 
-def january(request):
-    return HttpResponse("This is January")
+def monthlyChallenge(request, month):
+    challenge_text = None
+    if month == "january":
+        challenge_text = "This is January"
+    elif month == "february":
+        challenge_text = "This is February"
+    elif month == "march":
+        challenge_text = "This is March"
+    elif month == "april":
+        challenge_text = "This is April"
+    elif month == "may":
+        challenge_text = "This is May"
+    elif month == "june":
+        challenge_text = "This is June"
+    elif month == "july":
+        challenge_text = "This is July"
+    elif month == "august":
+        challenge_text = "This is August"
+    elif month == "september":
+        challenge_text = "This is September"
+    elif month == "october":
+        challenge_text = "This is October"
+    elif month == "november":
+        challenge_text = "This is November"
+    elif month == "december":
+        challenge_text = "This is December"
+    else:
+        return HttpResponseNotFound("Invalid month")
+    return HttpResponse("" + challenge_text)
 
-def february(request):
-    return HttpResponse("This is February")
-
-def march(request):
-    return HttpResponse("This is March")
-
-def april(request):
-    return HttpResponse("This is April")
-
-def may(request):
-    return HttpResponse("This is May")
-
-def june(request):
-    return HttpResponse("This is June")
-
-def july(request):
-    return HttpResponse("This is July")
-
-def august(request):  
-    return HttpResponse("This is August")
-
-def september(request):
-    return HttpResponse("This is September")  
-  
-def october(request):
-    return HttpResponse("This is October")
-
-def november(request):
-    return HttpResponse("This is November")
-
-def december(request):  
-    return HttpResponse("This is December")
 
 
