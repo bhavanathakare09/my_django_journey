599c1264e1dd02e2f2bb18943402e0b6909cd814
bhavanathakare09 <bhavanathakare09@gmail.com>
2025-06-17 13:03:19 -0400

    used reverse function to access url no hard coding.

--- challenges/urls.py
+++ challenges/urls.py
@@ -2,6 +2,9 @@ from django.urls import path
 from .import views
 
 urlpatterns = [
-    path("<month>", views.monthlyChallenge),
+    path("<int:month>", views.monthly_challenge_number),
+    path("<str:month>", views.monthlyChallenge, name="month-challenge"),
       
 ]
+
+
--- challenges/views.py
+++ challenges/views.py
@@ -1,37 +1,41 @@
 from django.shortcuts import render
-from django.http import HttpResponse, HttpResponseNotFound
+from django.http import HttpResponse, HttpResponseNotFound, HttpResponseRedirect
+from django.urls import reverse
 
 # Create your views here.
 
+challenges_dict = {
+    "january": "This is January",
+    "february": "This is February",
+    "march": "This is March",
+    "april": "This is April",
+    "may": "This is May",
+    "june": "This is June",
+    "july": "This is July",
+    "august": "This is August",
+    "september": "This is September",
+    "october": "This is October",
+    "november": "This is November",
+    "december": "This is December"
+}
+
+def monthly_challenge_number(request, month):
+    months = list(challenges_dict.keys())
+    if month > len(months):
+        return HttpResponseNotFound("Invalid Month")
+    redirtect_request = months[month - 1]
+    redirect_path = reverse("month-challenge" , args=[redirtect_request])
+    return HttpResponseRedirect(redirect_path)
+    
+    
+   
+
 def monthlyChallenge(request, month):
-    challenge_text = None
-    if month == "january":
-        challenge_text = "This is January"
-    elif month == "february":
-        challenge_text = "This is February"
-    elif month == "march":
-        challenge_text = "This is March"
-    elif month == "april":
-        challenge_text = "This is April"
-    elif month == "may":
-        challenge_text = "This is May"
-    elif month == "june":
-        challenge_text = "This is June"
-    elif month == "july":
-        challenge_text = "This is July"
-    elif month == "august":
-        challenge_text = "This is August"
-    elif month == "september":
-        challenge_text = "This is September"
-    elif month == "october":
-        challenge_text = "This is October"
-    elif month == "november":
-        challenge_text = "This is November"
-    elif month == "december":
-        challenge_text = "This is December"
-    else:
-        return HttpResponseNotFound("Invalid month")
-    return HttpResponse("" + challenge_text)
+    try:
+        challenge_text = challenges_dict[month]
+        return HttpResponse(challenge_text)
+    except:
+        return HttpResponseNotFound("Challenge for this month does not exist.")
 
 
 
